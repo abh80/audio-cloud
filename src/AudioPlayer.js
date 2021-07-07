ea05984547bf74e8f45b4d556f086ee5c6882281
abh80 <techixspotify@gmail.com>
2021-07-07 10:13:04 +0530

    new release

---
+++ src/AudioPlayer.js
@@ -0,0 +1,56 @@
+const { EventEmitter } = require("events");
+const { spawn } = require("child_process");
+const Speaker = require("speaker");
+class AudioPlayer extends EventEmitter {
+  constructor() {
+    super();
+  }
+  resume() {
+    if (!this.stream) return { data: { error: true, trace: "NO_PLAYING" } };
+    if (!this.isPaused) return { data: { error: true, trace: "ALR_PLAYING" } };
+    this.speaker = new Speaker({
+      channels: 2,
+      sampleRate: this.rate,
+      bitDepth: 16,
+    });
+    this.isPaused = false;
+    this.stream.pipe(this.speaker);
+    return true;
+  }
+  pause() {
+    if (!this.stream) return { data: { error: true, trace: "NO_PLAYING" } };
+    if (this.isPaused) return { data: { error: true, trace: "ALR_PAUSED" } };
+    this.stream.pause();
+    this.speaker.end();
+    this.isPaused = true;
+    return true;
+  }
+  startStream(
+    stream,
+    data = {
+      rate: 44000,
+    },
+    filters = []
+  ) {
+    if (this.playing) this.speaker.end();
+    this.rate = data.rate;
+    this.filters = filters;
+    this.speaker = new Speaker({
+      channels: 2,
+      bitDepth: 16,
+      sampleRate: data.rate,
+    });
+    const arr = ["-i", stream, "-f", "s16le", "-ac", "2", "pipe:1"];
+    if (filters && filters.length) arr.concat(["-af", filters.join(",")]);
+    const out = spawn("ffmpeg", arr);
+    out.stdout.pipe(this.speaker);
+    this.stream = out.stdout;
+    this.speaker.on("finish", () => {
+      if (this.isPaused) this.emit("stream-paused");
+      else this.emit("stream-end");
+    });
+    return this.speaker;
+  }
+}
+
+module.exports = AudioPlayer;
--- src/preload.js
+++ src/preload.js
@@ -1,10 +1,9 @@
 const SoundCloud = require("soundcloud-scraper");
 const SoundCloudClient = new SoundCloud.Client();
-const fs = require("fs");
-const Speaker = require("speaker");
+const Player = require("./AudioPlayer");
 
 const { contextBridge, ipcRenderer } = require("electron");
-const { spawn } = require("child_process");
+
 const wrapper = {
   async search(q, type = "all") {
     return await SoundCloudClient.search(q, type);
@@ -21,18 +20,13 @@ const wrapper = {
 };
 
 document.addEventListener("DOMContentLoaded", async () => {
-  //subscribeWindowControls();
+  try {
+    subscribeWindowControls();
+  } catch (e) {}
 
   SoundCloudClient.createAPIKey().then(async () => {
     contextBridge.exposeInMainWorld("scKey", SoundCloudClient.API_KEY);
     contextBridge.exposeInMainWorld("wrapper", wrapper);
-    const moosic = await wrapper.getInfo(
-      "https://soundcloud.com/bestcoversss/china-anuel-aa-daddy-yankee-karol-g-ozuna-j-balvin-conor-maynard-anth-cover"
-    );
-
-    const stream = await wrapper.getStream(moosic.streams.progressive);
-    const speaker = startStream(stream, { rate: 48000 }, ["bass=g=5", "dynaudnorm"]);
-
   });
 });
 
@@ -46,35 +40,4 @@ function subscribeWindowControls() {
   document.getElementById("min-btn").addEventListener("click", () => {
     ipcRenderer.invoke("min-pressed");
   });
-}
-
-function startStream(
-  stream,
-  data = {
-    rate: 44000,
-  },
-  filters = []
-) {
-  this.speaker = new Speaker({
-    channels: 2,
-    bitDepth: 16,
-    sampleRate: data.rate,
-  });
-  const arr = [
-    "-i",
-    stream,
-    "-af",
-    filters.join(","),
-    "-f",
-    "s16le",
-    "-ac",
-    "2",
-    "pipe:1",
-  ];
-  const out = spawn("ffmpeg", arr);
-  out.stdout.pipe(this.speaker);
-  this.speaker.once("close", () => {
-    console.log("closed");
-  });
-  return this.speaker;
 }
